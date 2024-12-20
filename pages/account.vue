6b746fc3a708c6f28b5c50b5748b3fafd3abe66d
QuantumParrot <ataraxia528491@gmail.com>
2024-12-21 07:53:06 +0800

    refactor: 將 middleware/redirect.js 移至 account 頁面中
    現階段判斷只有該頁面會使用它，故調整程式碼的位置

--- pages/account.vue
+++ pages/account.vue
@@ -1,6 +1,32 @@
 <script setup>
 
-definePageMeta({ layout: 'account', middleware: ['auth', 'redirect'] });
+import useAuthStore from '@/stores/auth';
+
+definePageMeta({
+    
+    layout: 'account',
+    
+    middleware: [
+        'auth',
+        (to, from) => {
+
+            if (import.meta.server) return;
+
+            const { showToastAlert } = useAlert();
+            const { isLoggedIn } = storeToRefs(useAuthStore());
+
+            const redirectPath = from.query.redirect && from.query.redirect.split('/')[1];
+
+            if (!isLoggedIn.value && (redirectPath === 'user' || redirectPath === 'admin')) {
+
+                showToastAlert({ icon: 'warning', text: '請重新登入' });
+
+            }
+
+        },
+    ]
+
+});
 
 //
 
