a38836d9da50a354800ae61c18b7183c26028fb0
QuantumParrot <ataraxia528491@gmail.com>
2024-12-24 02:49:08 +0800

    feat: 初步建置其他後台頁面

---
+++ pages/admin/news.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+
+<div>
+    <h2 class="fs-3 fw-bold text-primary-600 mb-10">消息管理</h2>
+</div>
+
+</template>
+
+<style scoped></style>
---
+++ pages/admin/room.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+
+<div>
+    <h2 class="fs-3">房型管理</h2>
+</div>
+
+</template>
+
+<style scoped></style>
